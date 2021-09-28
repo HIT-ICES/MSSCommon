db8cdc8a8c2b6f8494fd97ab4dc5c38ca9b9ab0e
SeptemberHX <september_hx@outlook.com>
2021-09-28 20:23:30 +0800

    add equal method for EndPoint

--- src/main/java/com/hitices/common/api/EndPoint.java
+++ src/main/java/com/hitices/common/api/EndPoint.java
@@ -3,6 +3,8 @@ package com.hitices.common.api;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 /**
  * @author septemberhx
  * @date 2021/9/6
@@ -20,5 +22,18 @@ public class EndPoint {
     public EndPoint(String ip, Integer port) {
         this.ip = ip;
         this.port = port;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EndPoint endPoint = (EndPoint) o;
+        return Objects.equals(ip, endPoint.ip) && Objects.equals(port, endPoint.port);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip, port);
     }
 }
